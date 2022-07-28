e8c324f8b7997c76faa05180ba9ab4689cee85a0
Thomas FAISCA <faisca.thomas@gmail.com>
2022-07-28 14:45:34 +0200

    Ajout de multer pour gestion des fichiers

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
-const auth = require('../backend/middleware/auth');
+const sauceRoutes = require('./routes/sauce');
+const path = require('path');
 
 const app = express();
 
@@ -19,23 +20,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauce', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-});
-
-
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
